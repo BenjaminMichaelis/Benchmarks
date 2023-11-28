4943cd8cc2d6455dafab19ffe1ad00a3a2b5af11
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2023-11-27 22:56:32 -0800

    Add New List vs Collection Initializer Benchmarks

---
+++ Benchmarks/BenchmarkClasses/NewListVsCollectionInitializer.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenjaminMichaelis.Benchmarks;
+
+public class NewListVsCollectionInitializer
+{
+    [Benchmark]
+    public List<string> NewList() => new() { "a", "b", "c" };
+
+    [Benchmark]
+    public List<string> CollectionInitializedList() => ["a", "b", "c"];
+}
--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -8,7 +8,7 @@ public class Program
     public static void Main(string[] args)
     {
         var config = DefaultConfig.Instance;
-        var summary = BenchmarkRunner.Run<LoggerVsLoggerMessage>(config, args);
+        var summary = BenchmarkRunner.Run<NewListVsCollectionInitializer>(config, args);
 
         // Use this to select benchmarks from the console:
         // var summaries = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
