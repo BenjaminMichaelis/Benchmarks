06ac0f36507dd9201c9372508bc88359afae2d6e
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2023-12-19 14:48:53 -0800

    Add Regex Benchmark

---
+++ Benchmarks/BenchmarkClasses/Regex/RegexCheck.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text.RegularExpressions;
+
+namespace Benchmarks.BenchmarkClasses.RegexCheck;
+
+[MemoryDiagnoser]
+public partial class RegexCheck
+{
+    private const string email = "test@intellitect.com";
+
+    [Benchmark]
+    public bool PatternPass_IsMatch() => Regex.IsMatch(email,
+                       @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                       RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+
+    [Benchmark]
+    public bool GeneratedRegex_IsMatch()
+    {
+        return EmailRegex().IsMatch(email);
+    }
+}
+partial class RegexCheck
+{
+    [GeneratedRegex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase)]
+    public static partial Regex EmailRegex();
+}
