3467c2cea11a2f65958f21b2ae3ae79b773f7fbd
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-05-21 12:55:38 -0700

    feat: Add To List vs Range Operator

---
+++ Benchmarks/BenchmarkClasses/ConditionalsBenchmarks/ToListVsRangeOperator.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenjaminMichaelis.Benchmarks.ConditionalsBenchmarks;
+
+[MemoryDiagnoser]
+public class ToListVsRangeOperator
+{
+    public IEnumerable<string> OriginalList = ["a", "b", "c"];
+
+    [Benchmark]
+    public List<string> ToList() => OriginalList.ToList();
+
+    [Benchmark]
+    public List<string> RangeOperator() => [.. OriginalList];
+}
